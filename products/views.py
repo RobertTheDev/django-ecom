ae9354244e4cbb9dbc24619ac9cfe23ef0fc3d56
RobertTheDev <robertthedev@gmail.com>
2024-01-01 12:27:16 +0000

    feat: created json data and mapped in products page

--- products/views.py
+++ products/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+import json
 
 # Create your views here.
 def index(request):
     template = loader.get_template('products/index.html')
-    return HttpResponse(template.render())
+    json_file_path = 'products/data.json'
+    with open(json_file_path, 'r') as json_file:
+        data = json.load(json_file)
+
+    context = {'products_data': data}
+    return HttpResponse(template.render(context))
 
 def product(request, product_id):
     template = loader.get_template('products/product.html')
