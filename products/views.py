36e656cce24709e204dd12b31926b088ead4b88b
RobertTheDev <robertthedev@gmail.com>
2024-01-01 12:06:27 +0000

    feat: created and defined product template and url

--- products/views.py
+++ products/views.py
@@ -6,3 +6,8 @@ from django.template import loader
 def index(request):
     template = loader.get_template('products/index.html')
     return HttpResponse(template.render())
+
+def product(request, product_id):
+    template = loader.get_template('products/product.html')
+    context = {'product_id': product_id}
+    return HttpResponse(template.render(context, request))
