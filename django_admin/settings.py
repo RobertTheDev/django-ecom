e6c32f2e185328c8bcbef9f9ecf7727a5589f93e
RobertTheDev <robertthedev@gmail.com>
2024-01-02 16:04:28 +0000

    feat: set up live reload

--- django_admin/settings.py
+++ django_admin/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'livereload',
     'django.contrib.staticfiles',
     'fontawesomefree',
     "compressor",
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'django_admin.urls'
